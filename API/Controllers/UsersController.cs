4e7632cd96e2fbcd8d1165e9a5cb225264ad5273
Khoi <110973167+marked01one@users.noreply.github.com>
2023-02-13 09:55:36 -0800

    Initial commit

---
+++ API/Controllers/UsersController.cs
@@ -0,0 +1,31 @@
+using API.Data;
+using API.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Controllers
+{
+  [ApiController]
+  [Route("api/{controller}")]
+  public class UsersController : ControllerBase
+  {
+    private readonly DataContext context;
+
+    public UsersController(DataContext context)
+    {
+      this.context = context;
+    }
+
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers()  // ActionResult<> allows you to return standard HTTP responses (i.e. NotFound())
+    {
+      return await this.context.Users.ToListAsync();
+    }
+
+    [HttpGet("{id}")]
+    public async Task<ActionResult<AppUser>> GetUser(int id)
+    {
+      return await this.context.Users.FindAsync(id);
+    }
+  }
+}
---
+++ API/Program.cs
@@ -0,0 +1,19 @@
+using API.Data;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddDbContext<DataContext>(options => 
+{
+  options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
+
+builder.Services.AddControllers();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+app.MapControllers();
+
+app.Run();
